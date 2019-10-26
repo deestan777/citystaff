30415243dad4f880f6b26e5a12d0c9300b8b7d1d
makeme.website <fosterluck777@gmail.com>
2019-10-27 00:59:25 +0300

    first commit

---
+++ js/app.js
@@ -0,0 +1,9 @@
+//Custom JS
+$(document).ready(function(){
+	$('a.target-burger').click(function(e){
+		$('div.container, nav.main-nav, a.target-burger').toggleClass('toggled');
+		e.preventDefault();
+	});//target-burger-click
+});//doc-rdy
+
+
