365748ba95dae722652977f3f90d17f3bec91492
Denysov Stanislav <44064013+deestan777@users.noreply.github.com>
2019-11-20 10:14:58 +0200

    Delete app.js

--- js/app.js
+++
@@ -1,9 +0,0 @@
-//Custom JS
-$(document).ready(function(){
-	$('a.target-burger').click(function(e){
-		$('div.container, nav.main-nav, a.target-burger').toggleClass('toggled');
-		e.preventDefault();
-	});//target-burger-click
-});//doc-rdy
-
-
